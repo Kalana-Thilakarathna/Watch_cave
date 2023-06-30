e0cd17f4af0a06ce170aa6b8cf726a3361c419c0
Kalana Thilakarathna <93968473+Kalana-Thilakarathna@users.noreply.github.com>
2023-06-30 21:35:57 +0530

    Update App.js

--- Front-End/watch_cave_web/src/App.js
+++ Front-End/watch_cave_web/src/App.js
@@ -3,9 +3,12 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">
+      <h1 className="text-h1 text-star font-bold underline hover:to-blue-500">
         Hello world!
       </h1>
+      <button className="bg-blue-300 text-green-800 hover:bg-blue-700 text-red-900 rounded px-3">
+        click
+      </button>
     </div>
   )
 }
