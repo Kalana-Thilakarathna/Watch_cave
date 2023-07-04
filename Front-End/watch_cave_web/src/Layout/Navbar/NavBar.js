bb575c9290c911301a120e5f88e25aa547b70139
Kalana Thilakarathna <93968473+Kalana-Thilakarathna@users.noreply.github.com>
2023-07-04 17:30:20 +0530

    40 ish

--- Front-End/watch_cave_web/src/Layout/Navbar/NavBar.js
+++ Front-End/watch_cave_web/src/Layout/Navbar/NavBar.js
@@ -1,17 +1,51 @@
 import React from 'react'
 import LOGO from '../../Images/logo.jpg'
-import { Link } from 'react-router-dom'
+import { Link,NavLink } from 'react-router-dom'
+import { ImSearch } from 'react-icons/im'
 
 
 function NavBar() {
+    const hover = "hover:text-subMain transition text-white";
+    const Hover = ({isActive}) => {
+      if (isActive === true){
+        return('text-subMain')
+      }
+        return(hover)
+    }
   return (
     <>
       <div className="bg-main shadow-md sticky top-0 z-20">
         <div className="container mx-auto py-6 px-2 lg:grid gap-10 grid-cols-7 justify-between items-center">
+
+          {/* logo section */}
           <div className="col-span-1 lg:block hidden">
             <Link to="/">
               <img src={LOGO} alt="logo" className="w-full h-20 object-contain" />
             </Link>
+          </div>
+
+          {/* search section */}
+          <div className="col-span-3">
+              <form className="w-full text-sm bg-dryGray rounded flex-btn gap-4">
+                <button
+                  type="submit"
+                  className="bg-subMain w-12 flex-colo h-12 rounded text-white"
+                >
+                  <ImSearch />
+                </button>
+                <input 
+                  type="text"
+                  placeholder="Search movie in here"
+                  className="font-medium placeholder:text-border text-sm w-11/12 h-12 bg-transparent border-none px-2 text-balance text-black"
+                />
+              </form>
+          </div>
+
+          {/* menus */}
+          <div className="col-span-3 font-medium text-sm hidden xl:gap-14 2xl: justify-between lg:flex xl:justify-end items-center">
+            <NavLink to="/movies" className={Hover}>
+              Movie
+            </NavLink>
           </div>
         </div>
       </div>
