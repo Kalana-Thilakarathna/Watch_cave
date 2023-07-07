c8b2cbc5d29ad2c82fd960208a3811d5efb07058
Kalana Thilakarathna <93968473+Kalana-Thilakarathna@users.noreply.github.com>
2023-07-07 12:45:19 +0530

    icons

--- Front-End/watch_cave_web/src/Components/Home/FlexMoviesItems.js
+++ Front-End/watch_cave_web/src/Components/Home/FlexMoviesItems.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {FaCalendarAlt} from 'react-icons/fa'
 import {GiSandsOfTime} from 'react-icons/gi'
-import {BiMoviePlay} from 'react-icons/bi'
+import {BiCategoryAlt} from 'react-icons/bi'
+
 
 function FlexMoviesItems({movie}) {
   return (
     <>
         <div className="flex items-center gap-2">
-            <BiMoviePlay className="text-subMain w-4 h-4" />
+          <BiCategoryAlt className="text-subMain w-3 h-3" />
             <span className="text-sm font-medium">{movie.category}</span>
         </div>
         <div className="flex items-center gap-2">
