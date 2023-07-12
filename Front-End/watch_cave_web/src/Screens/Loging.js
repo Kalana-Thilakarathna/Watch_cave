63afd8ded76344941a9280c6a95c64dcb8cd296b
Lasantha-chulabhaya <82959249+Lasantha-chulabhaya@users.noreply.github.com>
2023-07-12 10:14:49 +0530

    Login update and Register done

--- Front-End/watch_cave_web/src/Screens/Loging.js
+++ Front-End/watch_cave_web/src/Screens/Loging.js
@@ -6,7 +6,7 @@ function Loging() {
   return (
     <Layout>
       <div className="container mx-auto px-2 my-24 flex-colo">
-        <div className="w-full 2xl:w-2/5 gap-8 flex-colo p-14 md:w-3/5 bg-dry rounded-lg border border-border">
+        <div className="w-full 2xl:w-2/5 gap-8 flex-colo p-8 sm:p-14 md:w-3/5 bg-dry rounded-lg border border-border">
           <img src="/images/Logo.jpg" 
           alt="logo" 
           className="w-full h-12 object-contain">
