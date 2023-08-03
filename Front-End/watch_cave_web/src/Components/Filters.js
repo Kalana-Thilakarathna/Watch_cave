9e19c4f63a73f56037d36b87bb124636287901c2
Kalana Thilakarathna <93968473+Kalana-Thilakarathna@users.noreply.github.com>
2023-08-03 10:10:17 +0530

    update

--- Front-End/watch_cave_web/src/Components/Filters.js
+++ Front-End/watch_cave_web/src/Components/Filters.js
@@ -1,47 +1,48 @@
-// import React from 'react'
-// import { useState } from 'react'
+import React from 'react'
+import { useState } from 'react'
 
-// const YearData = [
-//     {title: "Sort By Year"},
-//     {title: "1700 - 1800"},
-//     {title: "1800 - 1900"},
-//     {title: "1900 - 2000"},
-//     {title: "2000 - 2010"},
-//     {title: "2010 - 2030"},
-//     ]
-// const TimesData = [
-//     {title: "Sort By Hours"},
-//     {title: "1 - 5 Hours"},
-//     {title: "5 - 10 Hours"},
-//     {title: "10 - 15 Hours"},
-//     {title: "15 - 20 Hours"},
-//     ]
+const YearData = [
+    {title: "Sort By Year"},
+    {title: "1700 - 1800"},
+    {title: "1800 - 1900"},
+    {title: "1900 - 2000"},
+    {title: "2000 - 2010"},
+    {title: "2010 - 2030"},
+    ]
+const TimesData = [
+    {title: "Sort By Hours"},
+    {title: "1 - 5 Hours"},
+    {title: "5 - 10 Hours"},
+    {title: "10 - 15 Hours"},
+    {title: "15 - 20 Hours"},
+    ]
 
-// const RatesData = [
-//     {title: "Sort By Rates"},
-//     {title: "1 Star"},
-//     {title: "2 Star"},
-//     {title: "3 Star"},
-//     {title: "4 Star"},
-//     {title: "5 Star"},
-//     ]
+const RatesData = [
+    {title: "Sort By Rates"},
+    {title: "1 Star"},
+    {title: "2 Star"},
+    {title: "3 Star"},
+    {title: "4 Star"},
+    {title: "5 Star"},
+    ]
 
-// function Filters() {
-//     const [category,setCatergory] = useState({title:"category"})
-//     const [year,setYear] = useState(YearData[0]);
-//     const [times,setTimes] = useState(YearData[0]);
-//     const [rates,setRates] = useState(YearData[0]);
+function Filters() {
+    const [category,setCatergory] = useState({title:"category"})
+    const [year,setYear] = useState(YearData[0]);
+    const [times,setTimes] = useState(YearData[0]);
+    const [rates,setRates] = useState(YearData[0]);
 
-//     const Filter = [
-//         {
-//             value:category,
-//             onChange:setCatergory,
-//             items:CategoriesData
-//         }
-//     ]
-//   return (
+    const Filter = [
+        {
+            value:category,
+            onChange:setCatergory,
+            items:CategoriesData
+        },
+        
+    ]
+  return (
 
-//   )
-// }
+  )
+}
 
-// export default Filters
+export default Filters
