95e79252a3b7ff1941711213325f39286abd9415
Kalana Thilakarathna <93968473+Kalana-Thilakarathna@users.noreply.github.com>
2023-08-04 01:21:20 +0530

    filter complete

--- Front-End/watch_cave_web/src/Components/Filters.js
+++ Front-End/watch_cave_web/src/Components/Filters.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
 import { CategoriesData } from '../Screens/Data/CategoriesData'
+import { Listbox, Transition } from '@headlessui/react'
+import {CgSelect} from 'react-icons/cg'
+import { Fragment } from 'react'
 
 const YearData = [
     {title: "Sort By Year"},
@@ -30,8 +33,8 @@ const RatesData = [
 function Filters() {
     const [category,setCatergory] = useState({title:"category"})
     const [year,setYear] = useState(YearData[0]);
-    const [times,setTimes] = useState(YearData[0]);
-    const [rates,setRates] = useState(YearData[0]);
+    const [times,setTimes] = useState(TimesData[0]);
+    const [rates,setRates] = useState(RatesData[0]);
 
     const Filter = [
         {
@@ -57,9 +60,49 @@ function Filters() {
     ]
   return (
         <div className="my-6 bg-dry text-dryGray border-gray-800 grid md:grid-cols-4 grid-cols-2 lg:gap-12 gap-2 rounded p-6">
-            
+            {
+                Filter.map((item,index) => (
+                    <Listbox key={item} value={item.value} onChange={item.onChange}>
+                        <div className="relative">
+                            <Listbox.Button className="relative border border-gray-800 w-full text-white py-4 pl-6 pr-10 text-left text-xs">
+                                <span className="block trucate">{item.value.title}</span>
+                                <span className="absolute inset-y-0 right-0 flex items-center pointer-events-none pr-2">
+                                    <CgSelect className="w-5 h-5" area-hidden="true"/>
+                                </span>
+                            </Listbox.Button>
+                            <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
+                                <Listbox.Options className="absolute z-10 mt-1 w-full bg-white border border-gray-800 text-dryGray rounded-md shadow-lg max-h-60 py-1 text-base ring-1 ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                                    {
+                                        item.items.map((iterm,i) => (
+                                            <Listbox.Option key={i} className={({active}) => `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? "bg-subMain text-white" : "text-main"}`
+                                            } value={iterm}>
+                                                {
+                                                    ({selected}) => (
+                                                        <>
+                                                        <span className={`block truncated ${selected ? 'font-semibold':'font-normal'}`}> 
+                                                            {iterm.title}
+                                                        </span>
+                                                        {
+                                                            // selected ? (
+                                                            //     <span className="absolute inset-y-0 left-0 flex items-center pl-3 ">
+                                                            //         <p>e</p>
+                                                            //     </span>
+                                                            // )
+                                                        }
+                                                        </>
+                                                    )
+                                                }
+                                            </Listbox.Option>
+                                        ))
+                                    }
+                                </Listbox.Options>
+                            </Transition>
+                        </div>
+                    </Listbox>
+                ))
+            }
         </div>
   )
 }
 
-export default Filters ;
+export default Filters
