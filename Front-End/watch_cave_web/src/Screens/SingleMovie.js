8eed7d0c8c97282f2dc86d9e2e6d24d9b82b7e4f
Kalana Thilakarathna <93968473+Kalana-Thilakarathna@users.noreply.github.com>
2023-08-08 21:56:14 +0530

    single move done

--- Front-End/watch_cave_web/src/Screens/SingleMovie.js
+++ Front-End/watch_cave_web/src/Screens/SingleMovie.js
@@ -5,11 +5,14 @@ import Movies from '../Screens/Data/MovieData'
 import MovieInfo from '../Components/Single/MovieInfo'
 import MovieRates from '../Components/Single/MovieRates'
 import Titles from '../Components/Titles'
+import { BiCollection } from 'react-icons/bi'
+import Movie from '../Components/Movie'
 
 
 function SingleMovie() {
   const {id} = useParams()
   const movie = Movies.find((movie) => movie.name === id)
+  const RelatedMovies = Movies.filter((m) => m.category === movie.category)
   return (
     <Layout>
       <MovieInfo movie={movie}/>
@@ -17,7 +20,14 @@ function SingleMovie() {
         {/* <MovieCast /> */}
         <MovieRates movie={movie} />
         <div className="my-16">
-          <Titles
+          <Titles title="Related Movies" Icon={BiCollection}></Titles>
+          <div className="grid sm:mt-10 mt-6 xl:grid-cols-4 2xl:grid-cols-5 lg:grid-cols-3 sm:grid-cols-2 gap-6">
+          {
+            RelatedMovies.map((movie,index) => (
+              <Movie movie={movie} key={index}/>
+            ))
+          }
+        </div>
         </div>
       </div>
 
